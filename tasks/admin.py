dd0d0a031fa4c4af767001311fc884136f6fa404
asi0723 <202380159@psu.palawan.edu.ph>
2026-03-15 21:49:56 +0800

    admin

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Priority, Category, Task, SubTask, Note
 
-# Register your models here.
+
+@admin.register(Priority)
+class PriorityAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status', 'deadline', 'priority', 'category']
+    list_filter = ['status', 'priority', 'category']
+    search_fields = ['title', 'description']
+
+
+@admin.register(SubTask)
+class SubTaskAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status', 'parent_task']
+    list_filter = ['status']
+    search_fields = ['title']
+
+
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ['task', 'created_at']
+    search_fields = ['content']
