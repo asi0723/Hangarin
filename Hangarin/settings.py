44f147ff2dd0d4046c7ec26feadb2cbbd9737179
asi0723 <202380159@psu.palawan.edu.ph>
2026-04-08 08:38:47 +0800

    Add PWA features

--- Hangarin/settings.py
+++ Hangarin/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'tasks',
     'crispy_forms',
     'crispy_bootstrap5',
+    'pwa',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,36 @@ SOCIALACCOUNT_PROVIDERS = {
         }
     }
 }
+
+# --- Progressive Web App Settings ---
+PWA_APP_NAME = 'ProjectSite'
+PWA_APP_DESCRIPTION = "A Progressive Web App version of ProjectSite"
+PWA_APP_THEME_COLOR = '#0A0A0A'
+PWA_APP_BACKGROUND_COLOR = '#FFFFFF'
+PWA_APP_DISPLAY = 'standalone'
+PWA_APP_SCOPE = '/'
+PWA_APP_ORIENTATION = 'portrait'
+PWA_APP_START_URL = '/'
+PWA_APP_STATUS_BAR_COLOR = 'default'
+PWA_APP_ICONS = [
+    {
+        'src': '/static/img/icon-192.png',
+        'sizes': '192x192'
+    },
+    {
+        'src': '/static/img/icon-512.png',
+        'sizes': '512x512'
+    }
+]
+PWA_APP_ICONS_APPLE = [
+    {
+        'src': '/static/img/icon-192.png',
+        'sizes': '192x192'
+    },
+    {
+        'src': '/static/img/icon-512.png',
+        'sizes': '512x512'
+    }
+]
+PWA_APP_DIR = 'ltr'
+PWA_SERVICE_WORKER_PATH = os.path.join(BASE_DIR, 'static/js', 'serviceworker.js')
